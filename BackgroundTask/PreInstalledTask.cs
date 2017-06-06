16f2f500b8d88ec412e2936dc40913da09cd806b
Pradithya <pradithya.aria@gmail.com>
2017-06-06 12:18:07 +0800

    Initial revision
    Add sample application with preinstall capability

---
+++ BackgroundTask/PreInstalledTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTask
+{
+    public sealed class PreInstalledTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            ToastHelper.PopToast("Pre-Install Task", "Pre install task is running", "OK");
+        }
+    }
+}
---
+++ BackgroundTask/ToastHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace BackgroundTask
+{
+    internal static class ToastHelper
+    {
+        public const string DEFAULT_GROUP = "trcagent-group";
+        public static ToastNotification PopToast(string title, string content, string buttonlabel, string tag = null, string group = DEFAULT_GROUP)
+        {
+            string xml = $@"<toast activationType='foreground' duration='long'>
+                                            <visual>
+                                                <binding template='ToastGeneric'>
+                                                </binding>
+                                            </visual>
+                                            <actions>
+                                            </actions>
+                                             </toast>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            var binding = doc.SelectSingleNode("//binding");
+
+            // add title
+            var el = doc.CreateElement("text");
+            el.InnerText = title;
+            binding.AppendChild(el);
+
+            // add content
+            el = doc.CreateElement("text");
+            el.InnerText = content;
+            binding.AppendChild(el);
+
+            // add button
+            var actions = doc.SelectSingleNode("//actions");
+            var action = doc.CreateElement("action");
+            action.SetAttribute("activationType", "foreground");
+            action.SetAttribute("content", buttonlabel);
+            action.SetAttribute("arguments", "OK");
+            actions.AppendChild(action);
+
+            var s = doc.GetXml();
+
+            return PopCustomToast(doc, tag, group);
+        }
+
+        private static ToastNotification PopCustomToast(XmlDocument doc, string tag, string group)
+        {
+            var toast = new ToastNotification(doc);
+
+            if (tag != null)
+                toast.Tag = tag;
+
+            if (group != null)
+                toast.Group = group;
+
+            ToastNotificationManager.History.Clear();
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+            return toast;
+        }
+    }
+}
